ba2e0a2d29442a816a9b5e1329816e4d5487739e
Jose Rios <68526429+snnare@users.noreply.github.com>
2024-04-05 21:06:42 -0600

    Add files via upload

---
+++ Front/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddEditAlumnoComponent } from './components/add-edit-alumno/add-edit-alumno.component';
+import { ListAlumnosComponent } from './components/list-alumnos/list-alumnos.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+// Animaciones:
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AddEditAlumnoComponent,
+    ListAlumnosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Front/src/app/services/alumno.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Alumno } from '../interfaces/alumno';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlumnoService {
+
+  private myAppUrl: string;
+  private myApiUrl: string;
+
+  constructor(private http: HttpClient) { 
+    this.myAppUrl = "http://localhost:4000/"
+    this.myApiUrl = "api/alumnos/"
+  }
+
+  obtenerTodosAlumnos(): Observable<Alumno[]> {
+    return this.http.get<Alumno[]>(this.myAppUrl + this.myApiUrl);
+  }
+
+  eliminarAlumno(id: number): Observable<void> {
+    return this.http.delete<void>(this.myAppUrl + this.myApiUrl + id);
+  }
+
+  guardarAlumno(alumno: Alumno): Observable<void> {
+    return this.http.post<void>(this.myAppUrl + this.myApiUrl, alumno);
+  }
+
+
+  obtenerAlumno(id: number): Observable<Alumno> {
+    return this.http.get<Alumno>(this.myAppUrl + this.myApiUrl + id);
+  }
+
+  actualizarAlumno(id: number, alumno: Alumno): Observable<void> {
+    return this.http.put<void>(this.myAppUrl + this.myApiUrl + id, alumno);
+  }
+  
+}
