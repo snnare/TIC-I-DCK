ab7f5154d0883723411d370f978be81c41d31d23
Jose Rios <68526429+snnare@users.noreply.github.com>
2024-04-11 15:36:46 -0600

    Update server.js

--- Back/dist/models/server.js
+++ Back/dist/models/server.js
@@ -8,16 +8,40 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const connectionDB_1 = __importDefault(require("../connectionDB/connectionDB"));
-const persona_routes_1 = __importDefault(require("../routes/persona-routes"));
-class Server {
-    constructor() {
+var express_1 = require("express");
+var cors_anywhere_1 = require("cors-anywhere");
+var connectionDB_1 = require("../connectionDB/connectionDB");
+var persona_routes_1 = require("../routes/persona-routes");
+var Server = /** @class */ (function () {
+    function Server() {
         this.app = (0, express_1.default)();
         this.port = '4000';
         this.listen();
@@ -25,33 +49,53 @@ class Server {
         this.routes();
         this.dbConnect();
     }
-    listen() {
-        this.app.listen(this.port, () => {
-            console.log('Corriendo  en: ', this.port);
+    Server.prototype.listen = function () {
+        var _this = this;
+        this.app.listen(this.port, function () {
+            console.log('Corriendo  en: ', _this.port);
         });
-    }
-    routes() {
-        this.app.get('/', (req, res) => {
+    };
+    Server.prototype.routes = function () {
+        this.app.get('/', function (req, res) {
             res.json({
                 msg: 'Api OK'
             });
         });
         this.app.use('/api/alumnos', persona_routes_1.default);
-    }
-    midlewares() {
+    };
+    Server.prototype.midlewares = function () {
         this.app.use(express_1.default.json());
-        this.app.use((0, cors_1.default)());
-    }
-    dbConnect() {
-        return __awaiter(this, void 0, void 0, function* () {
-            try {
-                yield connectionDB_1.default.authenticate();
-                console.log('BD OK');
-            }
-            catch (error) {
-                console.log(error);
-            }
+        //this.app.use(cors())
+        // Configuración de CORS Anywhere como middleware
+        this.app.use(function (req, res, next) {
+            cors_anywhere_1.default.createServer({
+                originWhitelist: [], // Permitir todos los orígenes
+                requireHeader: ['origin', 'x-requested-with'],
+                removeHeaders: ['cookie', 'cookie2']
+            }).handleRequest(req, res);
         });
-    }
-}
+    };
+    Server.prototype.dbConnect = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var error_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        _a.trys.push([0, 2, , 3]);
+                        return [4 /*yield*/, connectionDB_1.default.authenticate()];
+                    case 1:
+                        _a.sent();
+                        console.log('BD OK');
+                        return [3 /*break*/, 3];
+                    case 2:
+                        error_1 = _a.sent();
+                        console.log(error_1);
+                        return [3 /*break*/, 3];
+                    case 3: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    return Server;
+}());
 exports.default = Server;
